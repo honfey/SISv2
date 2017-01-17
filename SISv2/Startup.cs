6d77e5468d3657d60177afeaeea37fa920952f1d
honfe <honfe@DESKTOP-NF0SM14>
2017-01-17 13:24:52 +0800

    Add project files.

---
+++ SISv2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SISv2.Startup))]
+namespace SISv2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
